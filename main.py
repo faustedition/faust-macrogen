8ef616dac6efaee3b0c2c76fb5dcd5b92b8f80de
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2018-07-31 02:25:45 +0200

    Include new reports

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from faust_logging import logging
 import sys
 
 import graph
-from report import report_components, report_refs, report_missing
+from report import report_components, report_refs, report_missing, report_index, report_conflicts
 from visualize import render_all
 
 logger = logging.getLogger('main')
@@ -15,11 +15,12 @@ def _main(argv=sys.argv):
     report_refs(graphs)
     report_missing(graphs)
     report_components(graphs)
+    report_conflicts(graphs)
+    report_index()
     render_all()
 
 
 if __name__ == '__main__':
-    import requests_cache
-
-    requests_cache.install_cache(expire_after=86400)
+#    import requests_cache
+#    requests_cache.install_cache(expire_after=86400)
     _main()
