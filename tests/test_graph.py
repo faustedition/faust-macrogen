a8fb0ca6f97b7c08018c9c57d5d914bc8974bf08
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2020-03-27 18:27:37 +0100

    Mark some tests as slow

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -2,12 +2,16 @@ import macrogen
 from macrogen.config import config
 import pytest
 
+
 @pytest.fixture('session')
+@pytest.mark.slow
 def eades_graphs():
     config.fes_method = 'eades'
     config.lightweight_timeline = False
     return macrogen.graph.MacrogenesisInfo()
 
+
+@pytest.mark.slow
 def test_conflict_info(eades_graphs):
     cs = eades_graphs.conflict_stats()
     assert cs
