8d1a0304c3aefab3057f6a26cd59849a9610be34
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2023-03-01 18:42:48 +0100

    Improved error reporting and logging

--- src/graphviewer/gvfa.py
+++ src/graphviewer/gvfa.py
@@ -1,5 +1,6 @@
 import asyncio
 import codecs
+import logging
 from asyncio import create_subprocess_exec, wait_for
 from collections.abc import Mapping
 from dataclasses import dataclass
@@ -11,17 +12,38 @@ import networkx as nx
 import pydantic
 import uvicorn
 from fastapi import FastAPI, Depends, Request, Response, HTTPException
+from fastapi.exception_handlers import http_exception_handler
 from fastapi.templating import Jinja2Templates
+from fastapi.exceptions import HTTPException
 from macrogen import MacrogenesisInfo, write_dot
 from macrogen.graph import Node
 from macrogen.graphutils import remove_edges, expand_edges, simplify_timeline, collapse_parallel_edges
-from pydantic import BaseModel
+from macrogen.config import config
+from starlette.responses import HTMLResponse
+
+logger = logging.getLogger(__name__)
+
+from pydantic import BaseModel, BaseSettings
 from pygraphviz import AGraph
 
-app = FastAPI()
+
+class Settings(BaseSettings):
+    default_model: Path = Path('target/macrogenesis/macrogen-info.zip')
+    extra_models: dict[str, Path] | list[Path] | str = '*-*/macrogen-info.zip'
+    class Config:
+        env_file = '.env'
+
+settings = Settings()
+app = FastAPI(debug=True)
 
 S = TypeVar('S')
 T = TypeVar('T')
+
+
+@app.exception_handler(HTTPException)
+async def logging_exception_handler(request, exc):
+    logger.exception(exc)
+    return http_exception_handler(request, exc)
 
 
 class LazyLoader(Mapping):
@@ -93,29 +115,42 @@ MIME_TYPES: Dict[ExportFormat, str] = {
 
 @app.on_event('startup')
 def load_models():
-    # TODO configurability
     global models
 
-    model_files = list(Path('target').glob('*-*/macrogen-info.zip'))
-    by_key = {'default': Path('target/macrogenesis/macrogen-info.zip')}
-    by_key.update({p.parent.stem: p for p in model_files if p not in by_key.values()})
+    by_key = {'default': settings.default_model}
+    if isinstance(settings.extra_models, dict):
+        by_key.update({k: Path(v) for (k, v) in settings.extra_models.items()})
+    else:
+        if isinstance(settings.extra_models, list):
+            model_files = [Path(entry) for entry in settings.extra_models]
+        else:
+            model_files = [entry for entry in Path().glob(settings.extra_models)]
+        by_key.update({p.parent.stem: p for p in model_files if p not in by_key.values()})
 
     def load_model(key):
+        logger.info('Loading model %s ...', key)
         return MacrogenesisInfo(by_key[key])
 
     models = LazyLoader(load_model, by_key.keys())
+    logger.info('Found %d models: %s', len(by_key), by_key.keys())
 
 
 templates = Jinja2Templates(directory='src/graphviewer/templates')
 
 
-@app.get('/macrogenesis/subgraph')
-async def render_form(request: Request):
+@app.get('/macrogenesis/subgraph', response_class=HTMLResponse)
+async def render_form(request: Request) -> templates.TemplateResponse:
+    """
+    Returns a HTML page with the subgraph viewer’s frontend.
+    """
     return templates.TemplateResponse('form.html', {'request': request, 'models': models})
 
 
-@app.get('/macrogenesis/subgraph/help')
-async def render_form(request: Request):
+@app.get('/macrogenesis/subgraph/help', response_class=HTMLResponse)
+async def render_form(request: Request) -> templates.TemplateResponse:
+    """
+    Returns a HTML page with help on the options of the subgraph viewer.
+    """
     return templates.TemplateResponse('help.html', {'request': request, 'models': models})
 
 
@@ -233,15 +268,29 @@ def extract_dot(info: _AGraphInfo = Depends(agraph)) -> AGraphInfo:
                       unknown_nodes=info.unknown_nodes)
 
 
-@app.get('/macrogenesis/subgraph/dot')
-def render_dot(info: _AGraphInfo = Depends(agraph)):
+@app.get('/macrogenesis/subgraph/dot', responses={200: {'content': {'text/vnd.graphviz': {}}}})
+def render_dot(info: _AGraphInfo = Depends(agraph)) -> Response:
+    """
+    Returns a dot file with the (unlayouted) graph.
+    """
     return Response(content=info.graph.to_string(),
                     media_type='text/vnd.graphviz',
                     headers={'Content-Disposition': f'attachment; filename="{info.basename}.dot"'})
 
 
-@app.get('/macrogenesis/subgraph/{format}')
+@app.get('/macrogenesis/subgraph/{format}', responses={
+    404: {'description': 'None of the given nodes have been found in the graph.'},
+    513: {'description': 'The rendering process failed due to a GraphViz error.'},
+    504: {'description': 'The rendering process timed out, probably due to a too complex graph. '
+                         'You should get a dot file using the /dot endpoint and run GraphViz locally.'}
+})
 async def render_image(format: ExportFormat, agraph_info: _AGraphInfo = Depends(agraph)):
+    """
+    Layouts and renders a graph.
+    
+    When successful, this will return an image in the given format. Note that this shouldn’t be used
+    for too complex cases since it has quite a low timeout. 
+    """
     if not agraph_info.nodes:
         raise HTTPException(404, dict(error="empty", msg="No nodes in graph", unknown_nodes=agraph_info.unknown_nodes))
     proc = await create_subprocess_exec('dot', '-T', format.value, stdin=asyncio.subprocess.PIPE,
@@ -260,6 +309,10 @@ async def render_image(format: ExportFormat, agraph_info: _AGraphInfo = Depends(
                                       msg="This layout is to complex to be rendered within the server's limits.\n"
                                           "Download a .dot file and run graphviz on your local machine to get a rendering."))
 
+def run_dev_server():
+    print(logger.name)
+    uvicorn.run('gvfa:app', host="0.0.0.0", port=5000, reload=True, reload_dirs=["src/graphviewer"])
+
 
 if __name__ == '__main__':
-    uvicorn.run('gvfa:app', host="0.0.0.0", port=5000, reload=True, reload_dirs=["src/graphviewer"])
+    run_dev_server()
